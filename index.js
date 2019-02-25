751aaa3b8ea371a2ec737854e316e1686ffded9d
Goffert van Gool <ruphin@ruphin.net>
2019-02-25 03:36:06 +0100

    Progress

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 import '/node_modules/slidem/slidem-deck.js';
 import '/node_modules/slidem/slidem-slide.js';
-import '/node_modules/slidem/slidem-polymersummit-slide.js';
-import '/node_modules/slidem-codepen-slide/slidem-codepen-slide.js';
-import '/node_modules/@gluon/keybinding/gluon-keybinding.js';
